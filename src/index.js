43128f51e0bae2240d495a88d930a71af1d55f13
pashax0 <pashax0@mail.ru>
2019-03-14 23:40:40 +0300

    fix: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var strArr = str.split(''), i = 0, j = 0;
+    while (j < strArr.length) {
+        if (i < bracketsConfig.length) {
+            if (strArr[j] == bracketsConfig[i][0] && strArr[j + 1] == bracketsConfig[i][1]) {
+                strArr.splice(j, 2);
+                cycleNumb = 1;
+                i = 0;
+                j = 0;
+            } else {
+                i++;
+            }
+        } else {
+            i = 0;
+            j++;
+        }
+    }
+
+  return strArr.length == 0;
 }
